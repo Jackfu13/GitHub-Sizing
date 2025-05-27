46e03d06987535b079bc2fad1dfab4754b9fc2b1
Jack Fu <jackfu@synopsys.com>
2025-05-27 09:21:56 -0700

    pushes

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <Token>" \
+  -H "Authorization: Bearer <token>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -o src\\output.json \
   --url https://api.github.com/user/repos
