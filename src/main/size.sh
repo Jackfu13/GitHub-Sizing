4d8921b67edfecb3184cece442e8661ba33a3654
Jack Fu <jackfu@synopsys.com>
2025-05-29 15:17:34 -0700

    added input

--- src/main/size.sh
+++ src/main/size.sh
@@ -1,6 +1,6 @@
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer <token>" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -o output.json \
   --url https://api.github.com/user/repos
