ef8a2c35e022820ecdbe1d2aba236c3fc4004cec
Jack Fu <jackfu@synopsys.com>
2025-05-29 14:10:40 -0700

    input script

--- src/main/github_sizing_scriptv2.py
+++ src/main/github_sizing_scriptv2.py
@@ -4,37 +4,17 @@ import json
 import certifi
  # Retrieve token from environment variable
 token = input("Please enter your PAT token: ")
-    
+url = input("Enter the API endpoint url")
 headers = {'Authorization': f'token {token}'}
-response = requests.get('https://api.github.com/user/repos', headers=headers)
+response = requests.get(url, headers=headers)
 
 filter = []
 if response.status_code == 200:
     
     repos = response.json()
-
-    for repo in repos:
-    
-        response2 = requests.get(f"https://api.github.com/repos/{repo["owner"]["login"]}/{repo["name"]}/collaborators",headers = headers, verify=certifi.where())
-        clist = []
-        if response.status_code==200:
-            collaborators = response2.json()
-            for user in collaborators:
-                 clist.append(user["login"])
-        else:
-            print(f"Error: {response2.status_code}")
-            print(response2.text)
-            exit()
-            
-        reps = {}
-        reps["name"] = repo["name"]
-        reps["owner"] = repo["owner"]["login"]
-        reps["size"] = repo["size"]
-        reps["collaborators"] = clist
-        filter.append(reps)
     with open('output//data.json', 'w') as file:
         json.dump(filter, file, indent=4)
-    
+    print("json saved to output/data.json")
 else:
         print(f"Error: {response.status_code}")
         print(response.text)
