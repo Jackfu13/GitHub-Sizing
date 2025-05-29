b4c8a098b5e1caa3d9a342e1312c3caab37b9649
Jack Fu <jackfu@synopsys.com>
2025-05-29 15:19:16 -0700

    input

---
+++ src/main/generic_script.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import json
+import certifi
+ # Retrieve token from environment variable
+token = input("Enter your PAT token: ")
+url = input("Enter the API endpoint url: ")
+headers = {'Authorization': f'token {token}'}
+response = requests.get(url, headers=headers)
+
+
+
+
+if response.status_code == 200:
+    
+    repos = response.json()
+    with open('filterdata.json', 'w') as file:
+        json.dump(repos, file, indent=4)
+    print("json saved to data.json")
+else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
