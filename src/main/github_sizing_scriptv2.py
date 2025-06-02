52f0396b404597bddb7129be53b77f49650f3bf6
Jack Fu <jackfu@synopsys.com>
2025-06-02 16:35:36 -0700

    one script

--- src/main/github_sizing_scriptv2.py
+++ src/main/github_sizing_scriptv2.py
@@ -15,7 +15,7 @@ if response.status_code == 200:
     with open('rawdata.json', 'w') as file:
         json.dump(repos, file, indent=4)
     print("Saved to rawdata.json")
-    answer = input("Filter data(name,owner,size?) Y or N(default)")
+    answer =  input("Filter data(name,size,owner)? Y or N, No is default")
     if answer.lower()=="y":
         filter = []
         for repo in repos:
@@ -26,7 +26,7 @@ if response.status_code == 200:
             filter.append(reps)
         with open('filterdata.json', 'w') as file:
             json.dump(filter, file, indent=4)
-        print("saved to filterdata.json")
+        print("json saved to filterdata.json")
 else:
         print(f"Error: {response.status_code}")
         print(response.text)
--- src/test/collab.sh
+++
@@ -1,6 +0,0 @@
-curl -L \ 
-  -H "Accept: application/vnd.github+json" \ 
-  -H "Authorization: Bearer <token>" \ 
-    -H "X-GitHub-Api-Version: 2022-11-28" \ 
-    -o output\output.json \ 
-    --url https://api.github.com/user/repos
