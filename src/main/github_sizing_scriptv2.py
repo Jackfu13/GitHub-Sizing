c7183bfa9a91fa5e7dcca38649f84b1ec398b956
Jack Fu <jackfu@synopsys.com>
2025-06-11 11:49:13 -0700

    gitlab script

--- src/main/github_sizing_scriptv2.py
+++ src/main/github_sizing_scriptv2.py
@@ -12,21 +12,19 @@ response = requests.get(url, headers=headers,verify=False)
 if response.status_code == 200:
     
     repos = response.json()
-    with open('rawdata.json', 'w') as file:
+    with open('rawgithubdata.json', 'w') as file:
         json.dump(repos, file, indent=4)
-    print("Saved to rawdata.json")
-    answer = input("Filter data(name,owner,size)? Y or N, default is No")
-    if answer.lower()=="y":
-        filter = []
-        for repo in repos:
-            reps = {}
-            reps["name"] = repo["name"]
-            reps["owner"] = repo["owner"]["login"]
-            reps["size"] = repo["size"]
-            filter.append(reps)
-        with open('filterdata.json', 'w') as file:
-            json.dump(filter, file, indent=4)
-        print("saved to filterdata.json")
+    print("Saved to rawgithubdata.json")
+    filter = []
+    for repo in repos:
+        reps = {}
+        reps["name"] = repo["name"]
+        reps["owner"] = repo["owner"]["login"]
+        reps["size"] = repo["size"]
+        filter.append(reps)
+    with open('githubdata.json', 'w') as file:
+        json.dump(filter, file, indent=4)
+    print("saved to githubdata.json")
 else:
         print(f"Error: {response.status_code}")
         print(response.text)
--- src/main/gitlabscript.py
+++ src/main/gitlabscript.py
@@ -5,14 +5,32 @@ import certifi
  # Retrieve token from environment variable
  #token = input("Enter your PAT token: ")
 #url = input("Enter the API endpoint url: ")
-token = "<token>"
+#token = "glpat-Z6H1KV7CNvW_rakP5q3c"
+token = input("Please enter your Personal Access Token: ")
+url = input("Please enter the API endpoint: ")
 headers = {'PRIVATE-TOKEN': f'{token}'}
 params = {"membership": True, "per_page": 100,"statistics":True}
-response = requests.get(url = "https://gitlab.com/api/v4/projects", headers=headers,verify=False,params = params)
+response = requests.get(url, headers=headers,verify=False,params = params)
 
 
 if response.status_code == 200:
     data = response.json()
-    with open('test.json', 'w') as file:
+    with open('rawgitlabdata.json', 'w') as file:
         json.dump(data, file, indent=4)
+    print("Raw data saved to gitlabdata.json")
+    statList = []
+    for repo in data:
+        dict = {}
+        dict["id"] = repo["id"]
+        dict["projectName"] = repo["name"]
+        dict["owner"] = repo["namespace"]["name"]
+        dict["statistics"] = repo["statistics"]
+        statList.append(dict)
+
+    with open('gitlabdata.json', 'w') as file:
+        json.dump(statList, file, indent=4)
+        print("Filtered data saved to gitlabdata.json")
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
   
